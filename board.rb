18ffc3fbfe9460cbc86e1d47e1ea18cc76ccd619
jinfull <jinfull@gmail.com>
2019-02-25 14:09:21 -0800

    Added coordinate bracket methods to view and set position on board.

--- board.rb
+++ board.rb
@@ -1,22 +1,38 @@
+require_relative "piece"
+
 class Board
     attr_reader :board
 
     def initialize
         @board = Array.new(8) { Array.new(8) }
 
-        @board.each do |row|
-            row.each do |col|
+        (0..7).each do |row|
+            (0..7).each do |col|
                 if (0..1).include?(row) || (6..7).include?(row)
                     @board[row][col] = Piece.new
-                else
-                    @board[row][col] = nil
                 end
             end
         end
     end
 
+    def move_piece(start_pos, end_pos)
+        begin
+            
+        rescue => exception
+            
+        end
 
 
+    end
 
+    
+
+    def [](row,col)
+        @board[row][col]
+    end
+
+    def []=(row,col,value) 
+        @board[row][col] = value
+    end
 
 end
