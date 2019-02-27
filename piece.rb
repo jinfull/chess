18b2f11daa2fa9d2e48c4bfccbd0fff5425d01a3
jinfull <jinfull@gmail.com>
2019-02-26 22:54:23 -0800

    Added skeleton methods for every piece type subclass as well as sliding/stepping modules.

--- piece.rb
+++ piece.rb
@@ -9,3 +9,55 @@ class Piece
         # returns an array of positions a Piece can move to
     end
 end
+
+module Sliding
+
+end
+
+module Stepping
+
+end
+
+
+
+    # sliding pieces
+# need to implement sliding module?
+
+class Bishop < Piece
+
+end
+
+class Rook < Piece
+
+
+end
+
+class Queen < Piece
+
+end
+
+# stepping pieces
+# need to implement stepping module?
+
+class Knight < Piece
+
+end
+
+class King < Piece
+
+
+end
+
+# null pieces (occupy empty spaces)
+
+class NullPiece < Piece
+
+
+end
+
+# PAWNs
+
+class Pawn < Piece
+
+
+end
