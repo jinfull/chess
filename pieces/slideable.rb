f0affe312a3357c89840d7b19ab6051414c00b80
jinfull <jinfull@gmail.com>
2019-02-28 11:20:16 -0800

    Fixed slideable modules to be able to 'move' (eat) into spaces with enemy positions, doesn't let movement continue past that enemy controlled position.

--- pieces/slideable.rb
+++ pieces/slideable.rb
@@ -4,14 +4,15 @@ module Slideable
     def moves
         all_moves = []
         
-        # debugger
-
         move_dirs.each do |move_dir|
             move_diff = DIRECTIONS[move_dir]
             end_pos = [position.first + move_diff.first, position.last + move_diff.last]
             
-            while valid_move?(end_pos)
+            while in_bounds?(end_pos) && (empty_space?(end_pos) || enemy_in_pos?(end_pos))
                 all_moves << end_pos
+
+                break if enemy_in_pos?(end_pos)
+
                 end_pos = [end_pos.first + move_diff.first, end_pos.last + move_diff.last]
             end
         end
@@ -19,7 +20,16 @@ module Slideable
         all_moves
     end
 
-    def valid_move?(move)
-        (0..7).include?(move.first) && (0..7).include?(move.last) && @board[*move].is_a?(NullPiece)
+    def in_bounds?(move)
+        (0..7).include?(move.first) && (0..7).include?(move.last)
     end
+
+    def empty_space?(move)
+        @board[*move].is_a?(NullPiece)
+    end
+
+    def enemy_in_pos?(move)
+        !@board[*move].is_a?(NullPiece) && @board[*move].color != self.color
+    end
+
 end
